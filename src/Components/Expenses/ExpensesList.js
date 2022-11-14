a728776ae7b4191acbf51468a8c69a1876d9d21a
sunilbalami <skstha77@gmail.com>
2022-11-14 14:17:32 +1100

    Simple Expense record app with dynamic chart data.

---
+++ src/Components/Expenses/ExpensesList.js
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expense</h2>;
+  }
+  return (
+    <ul className="expenses_list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+export default ExpensesList;
---
+++ src/Components/NewExpense/NewExpense.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button on onClick={startEditingHandler}>
+          Add new Expenses
+        </button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
+};
+export default NewExpense;
